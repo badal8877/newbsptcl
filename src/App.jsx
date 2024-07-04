0dec27e0210b11ace3b6acfe9bff2eb0a6ffb86f
badal8877 <badal110059@gmail.com>
2024-07-04 18:14:02 +0530

    ok

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+// import NavigationBar from '../components/NavigationBar'
+// import Slider from '../components/Slider'
+import BiharMap from '../components/BiharMap'
+// import Test from '../components/test'
+// import Map from '../components/Map'
+// import TestBihar from '../components/TestBihar'
+
+function App() {
+
+  return (
+    <>
+    {/* <NavigationBar/>
+    <Slider/> */}
+    <BiharMap/>
+    {/* <Test/>
+    <Map/> */}
+    {/* <TestBihar/> */}
+    </>
+  )
+}
+
+export default App
